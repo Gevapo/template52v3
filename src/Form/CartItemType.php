6d97fefa86f40bb9cbf81d8ca55f3bf593f9cf90
Gevapo <software@gevapo.be>
2021-02-05 19:03:14 +0100

    forms, template && controller

--- src/Form/CartItemType.php
+++ src/Form/CartItemType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\CartItem;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,12 +13,22 @@ class CartItemType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        /** @var CartItem $cartItem */
+        $cartItem = $builder->getData();
+
         $builder
             ->add('amount', null, [
                 'label' => 'Aantal'
             ])
+            ->add('cartOptions', CollectionType::class, [
+                'label' => false,
+                'entry_type' => CartOptionType::class,
+                'entry_options' => [
+                    'label' => false,
+                ],
+            ])
 //            ->add('product')
-            ->add('In Winkelwagen', SubmitType::class)
+//            ->add("In winkelwagen", SubmitType::class)
         ;
     }
 
